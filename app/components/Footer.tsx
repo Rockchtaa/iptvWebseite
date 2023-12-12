901b8c3709244f70ddab92598bd6213c1d1dce1a
YounessHassoune <yhassoune7@gmail.com>
2023-12-12 18:34:33 +0100

    footer

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -1,65 +1,48 @@
 import React from "react";
 import Logo from "./Logo";
+import Link from "next/link";
+import headerNavLinks from "../data/headerNavLinks";
 
 function Footer() {
   return (
     <footer className="p-4 md:p-8 lg:p-10 bg-primary-950/10">
       <div className="mx-auto max-w-screen-xl text-center">
-       
-        <a href="/" className="flex items-center justify-center space-x-3 m-auto">
-              <Logo  className="-translate-y-1 w-12 h-12"/>
-              <span className=" text-2xl font-semibold whitespace-nowrap dark:text-white">
-                IPTV
-              </span>
-            </a>
+        <a
+          href="/"
+          className="flex items-center justify-center space-x-3 m-auto"
+        >
+          <Logo className="-translate-y-1 w-12 h-12" />
+          <span className=" text-2xl font-semibold whitespace-nowrap dark:text-white">
+            IPTV
+          </span>
+        </a>
         <p className="my-6 text-gray-500 dark:text-gray-400">
-          Open-source library of over 400+ web components and interactive
-          elements built for better web.
+          Enjoy your time with excellent image quality up to 4K, on any device,
+          anytime and anywhere, with over +18,000 channels, +88,000 VOD and
+          uptime 100%
         </p>
         <ul className="flex flex-wrap justify-center items-center mb-6 text-gray-900 dark:text-white">
-          <li>
-            <a href="#" className="mr-4 hover:underline md:mr-6 ">
-              About
-            </a>
-          </li>
-          <li>
-            <a href="#" className="mr-4 hover:underline md:mr-6">
-              Premium
-            </a>
-          </li>
-          <li>
-            <a href="#" className="mr-4 hover:underline md:mr-6 ">
-              Campaigns
-            </a>
-          </li>
-          <li>
-            <a href="#" className="mr-4 hover:underline md:mr-6">
-              Blog
-            </a>
-          </li>
-          <li>
-            <a href="#" className="mr-4 hover:underline md:mr-6">
-              Affiliate Program
-            </a>
-          </li>
-          <li>
-            <a href="#" className="mr-4 hover:underline md:mr-6">
-              FAQs
-            </a>
-          </li>
-          <li>
-            <a href="#" className="mr-4 hover:underline md:mr-6">
-              Contact
-            </a>
-          </li>
+          {headerNavLinks.map((link) => (
+            <li key={link.title}>
+              <Link
+                href={link.href}
+                className="hidden sm:block font-medium hover:cursor-pointer mr-4 hover:underline md:mr-6 "
+              >
+                {link.title}
+              </Link>
+            </li>
+          ))}
         </ul>
-        <span className="text-sm text-gray-500 sm:text-center dark:text-gray-400">
-          © 2021-2022{" "}
-          <a href="#" className="hover:underline">
-            Logo
+        <div className="flex  justify-center text-sm text-gray-500 sm:text-center dark:text-gray-400 space-x-2">
+          <span>Copyright © 2023 |</span>
+          <a href="/" className="flex items-center justify-center gap-2">
+            <Logo className=" w-4 h-4" />
+            <span className=" text-sm font-semibold whitespace-nowrap dark:text-white">
+              IPTV
+            </span>
           </a>
-          . All Rights Reserved.
-        </span>
+          <span>- Buy Best Premium Subscription</span>
+        </div>
       </div>
     </footer>
   );
